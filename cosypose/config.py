63454da093def6ab14fc4a8a5e8f02d69f19d51c
zhenpeng ge <zhenpeng.ge@qq.com>
2021-12-03 21:24:25 +0800

    update

--- cosypose/config.py
+++ cosypose/config.py
@@ -3,8 +3,12 @@ import os
 import yaml
 
 from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
 
-LOCAL_DATA_DIR = LOCAL_DATA_DIR=Path("/home/ubuntu/gezp/dataset/cosypose/local_data")
+# local_data_path = os.path.join(get_package_share_directory('cosypose'), 'local_data')
+local_data_path = "/home/ubuntu/gezp/dataset/cosypose/local_data"
+
+LOCAL_DATA_DIR = Path(local_data_path)
 
 
 assert LOCAL_DATA_DIR.exists()
